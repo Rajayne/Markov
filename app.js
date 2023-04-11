8fb7dbbe54283eaf15c268fc4fe26d2458c4fda9
Rajayne <ryann.j.nealey@gmail.com>
2023-04-11 07:46:42 -0700

    Function to generate text from text

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 /** Command-line tool to generate Markov text. */
+const fs = require('fs');
+const markov = require('./markov');
+const axios = require('axios');
+const process = require('process');
+
+function generateText(text) {
+    let mm = new markov.MarkovMachine(text);
+    console.log(mm.makeText());
+}
+
+function makeText(path) {
+}
